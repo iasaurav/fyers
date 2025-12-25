0667b31bd45a937ee6060c0d56980373a6083696
iasaurav <101176678+iasaurav@users.noreply.github.com>
2025-12-25 13:16:04 +0530

    Update ez.py

--- ez.py
+++ ez.py
@@ -9,6 +9,5 @@ pairs = [('NFO-OPT','NIFTY'),('NFO-OPT','BANKNIFTY'),('NFO-OPT','FINNIFTY'),
 option = pd.concat([df[(df.segment==s)&(df.name==n)].head(1) for s,n in pairs])
 option['expiry'] = pd.to_datetime(option['expiry']).dt.strftime('%d-%m-%Y')
 option = option.sort_values(by='expiry')
-option=option.drop(columns=['instrument_token','tick_size', 'last_price'])
 print(option)
 
