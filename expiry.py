9107de7ae80dab16bdca728a933f755dd187dbc7
iasaurav <101176678+iasaurav@users.noreply.github.com>
2026-01-08 14:50:15 +0530

    Refactor DataFrame variable names from df to af

--- expiry.py
+++ expiry.py
@@ -1,14 +1,14 @@
 
 import pandas as pd
 
-df = pd.read_csv('https://api.kite.trade/instruments')
-df=df[['name','segment','expiry','lot_size','tradingsymbol','strike','instrument_type','instrument_token','exchange_token','exchange']]
+af = pd.read_csv('https://api.kite.trade/instruments')
+af=af[['name','segment','expiry','lot_size','tradingsymbol','strike','instrument_type','instrument_token','exchange_token','exchange']]
 pairs = [('NFO-OPT','NIFTY'),('NFO-OPT','BANKNIFTY'),('NFO-OPT','FINNIFTY'),
          ('BFO-OPT','SENSEX'),('BFO-OPT','BANKEX'),('MCX-OPT','CRUDEOIL')]
 
-option = pd.concat([df[(df['segment']==segment) & (df['name']==name)].head(1)
+df = pd.concat([af[(af['segment']==segment) & (af['name']==name)].head(1)
                 for segment,name in pairs], ignore_index=True)
-option['expiry'] = pd.to_datetime(option['expiry']).dt.strftime('%d-%m-%Y')
-option = option.sort_values(by='expiry')
-print(option[['name','segment','expiry','lot_size']])
+df['expiry'] = pd.to_datetime(option['expiry']).dt.strftime('%d-%m-%Y')
+df = df.sort_values(by='expiry')
+print(df[['name','segment','expiry','lot_size']])
 
