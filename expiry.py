4f40954bb414f93ef7bf5ce5efa3a2cd40a677d4
iasaurav <101176678+iasaurav@users.noreply.github.com>
2026-01-08 14:42:19 +0530

    Update expiry.py

--- expiry.py
+++ expiry.py
@@ -6,7 +6,8 @@ df=df[['name','segment','expiry','lot_size','tradingsymbol','strike','instrument
 pairs = [('NFO-OPT','NIFTY'),('NFO-OPT','BANKNIFTY'),('NFO-OPT','FINNIFTY'),
          ('BFO-OPT','SENSEX'),('BFO-OPT','BANKEX'),('MCX-OPT','CRUDEOIL')]
 
-option = pd.concat([df[(df.segment==s)&(df.name==n)].head(1) for s,n in pairs])
+option = pd.concat([df[(df['segment']==segment) & (df['name']==name)].head(1)
+                for segment,name in pairs], ignore_index=True)
 option['expiry'] = pd.to_datetime(option['expiry']).dt.strftime('%d-%m-%Y')
 option = option.sort_values(by='expiry')
 print(option[['name','segment','expiry','lot_size']])
